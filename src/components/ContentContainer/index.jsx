a1b8a18e0cd4f6e8cbc22113251a2e6e4ae6bc0f
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-02-18 16:31:02 -0500

    add content container

--- src/components/ContentContainer/index.jsx
+++ src/components/ContentContainer/index.jsx
@@ -1,12 +1,77 @@
-import TopNavigation from "../TopNavigation"
+import TopNavigation from "../TopNavigation";
+import { BsPlusCircleFill } from 'react-icons/bs';
 
 const ContentContainer = () => {
   return (
     <div className="content-container">
       <TopNavigation />
-      ContentContainer
+      <div className="content-list">
+        <Post
+          name='Ada'
+          timestamp='one week ago'
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name='Chris'
+          timestamp='4 days ago'
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name='Ada'
+          timestamp='one week ago'
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name='Chris'
+          timestamp='4 days ago'
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
       </div>
+      <BottomBar />
+    </div>
   )
 }
+
+const Post = ({ name, timestamp, text }) => {
+
+  const seed = Math.round(Math.random() * 100);
+  return (
+    <div className={'post'}>
+      <div className='avatar-wrapper'>
+        <img src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`} alt='' className='avatar' />
+      </div>
+
+      <div className='post-content'>
+        <p className='post-owner'>
+          {name}
+          <small className='timestamp'>{timestamp}</small>
+        </p>
+        <p className='post-text'>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const BottomBar = () => (
+  <div className='bottom-bar'>
+    <BsPlusCircleFill
+      size='22'
+      className='text-green-500 dark:shadow-lg mx-2 dark:text-primary'
+    />
+    <input type='text' placeholder='Enter message...' className='bottom-bar-input' />
+  </div>
+);
 
 export default ContentContainer
